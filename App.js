f72da66492a25b95406326212648af52f565b859
muhammed umar <muhammedumaralx@gmail.com>
2023-08-30 23:07:15 +0100

    successfully connected to openai API

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 const userInput = document.getElementById("user-input")
 const sendButton = document.getElementById("send")
 const chatDisplay = document.querySelector('.chatDisplay ul')
+const API_KEY = "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";
 
 const handleInput = () => {
     const message = userInput.value.trim();
+    generateResponse(message);
     
     if (message !== ''){
         const userMessageItem = document.createElement('li');
@@ -16,7 +18,33 @@ const handleInput = () => {
         chatDisplay.appendChild(userMessageItem);
 
         userInput.value = "";
+        
     }
+}
+
+const generateResponse = (userMessage) => {
+    const API_URL = "https://api.openai.com/v1/chat/completions"
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: userMessage}]
+        })
+    }
+
+    fetch(API_URL, requestOptions)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => {
+        console.log(error)
+    })
 }
 
 sendButton.addEventListener("click", handleInput)
